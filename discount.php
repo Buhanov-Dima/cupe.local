c1dc1cf63f0c817273046707d511dc891c6620ea
Buhanov-Dima <35217770+Buhanov-Dima@users.noreply.github.com>
2018-09-03 02:19:32 +0500

    form (discount)

--- discount.php
+++ discount.php
@@ -35,7 +35,23 @@
 					</div>
 					<div class="wrapper-skidka">
 						<h1>НОВОГОДНЯЯ АКЦИЯ</h1>
-						<a href="#" class="btn-rmore">Узнать подробнее</a>
+						<a data-fancybox data-src="#skidka-1" href="javascript:;" class="btn-rmore">Узнать подробнее</a>
+
+						<div id="skidka-1" class="hidden-skidka">
+				       		<div class="wrap-main-rightbar">
+								<form action="" class="form-skidka">
+									<div class="txt-skidon">
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ea, nam fugiat aut necessitatibus dolorum ipsam voluptate reiciendis minima non natus. Soluta quaerat fuga amet omnis est pariatur voluptatem cumque!</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, aliquam aliquid, quam ab tempora molestiae repellendus a voluptate ut ullam accusantium nam, illum et magnam. Dicta laborum quibusdam, nemo modi!</p>
+									</div>
+									<h3>Получите <span>БЕСПЛАТНУЮ</span><br> консультацию специалиста</h3>
+									<input type="text" name="" class="m-name" placeholder="Ваше имя">
+									<input type="text" name="" class="m-phone" placeholder="Ваш телефон">
+									<button class="btn-form">Оставить заявку</button>
+									<p>«Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой конфиденциальности»</p>
+								</form>
+							</div>
+				       	</div>
 					</div>
 				</div>
 			</div>
@@ -48,7 +64,23 @@
 					</div>
 					<div class="wrapper-skidka">
 						<h1>НОВОГОДНЯЯ АКЦИЯ</h1>
-						<a href="#" class="btn-rmore">Узнать подробнее</a>
+						<a data-fancybox data-src="#skidka-2" href="javascript:;" class="btn-rmore">Узнать подробнее</a>
+
+						<div id="skidka-2" class="hidden-skidka">
+				       		<div class="wrap-main-rightbar">
+								<form action="" class="form-skidka">
+									<div class="txt-skidon">
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum ea, nam fugiat aut necessitatibus dolorum ipsam voluptate reiciendis minima non natus. Soluta quaerat fuga amet omnis est pariatur voluptatem cumque!</p>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, aliquam aliquid, quam ab tempora molestiae repellendus a voluptate ut ullam accusantium nam, illum et magnam. Dicta laborum quibusdam, nemo modi!</p>
+									</div>
+									<h3>Получите <span>БЕСПЛАТНУЮ</span><br> консультацию специалиста</h3>
+									<input type="text" name="" class="m-name" placeholder="Ваше имя">
+									<input type="text" name="" class="m-phone" placeholder="Ваш телефон">
+									<button class="btn-form">Оставить заявку</button>
+									<p>«Нажимая на кнопку, вы даете согласие на обработку персональных данных и соглашаетесь c политикой конфиденциальности»</p>
+								</form>
+							</div>
+				       	</div>
 					</div>
 				</div>
 			</div>
